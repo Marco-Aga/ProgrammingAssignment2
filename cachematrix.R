41e98e8f65e71c8db33d4401814ef6ce789a158a
Marco-Aga <102923235+Marco-Aga@users.noreply.github.com>
2025-05-27 18:12:41 +0200

    Implement makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Foreword: the comments have been translated from Italian to English,
+# so I apologize for any translation errors.
 
-## Write a short comment describing this function
+
+# As requested and as per example, I create this "special" matrix
+# that allows to cache the inverse of the matrix and itself.
+# Obviously I have to define the getter and setter functions for both the matrix
+# and the inverse, using the <<- operator to implement the caching.
 
 makeCacheMatrix <- function(x = matrix()) {
+    # cachedInverse will contain the inverse of the matrix, initialized to NULL
+    cachedInverse <- NULL  
 
+    # Function to set the array with the caching method
+    set <- function(y) {
+        x <<- y                # update the matrix
+        cachedInverse <<- NULL # reset cached inverse
+    }
+
+    # Getter function to get the matrix
+    get <- function() x
+
+    # Function to set the inverse from outside
+    setinverse <- function(inverse) cachedInverse <<- inverse
+
+    # Function to get the inverse
+    getinverse <- function() cachedInverse
+
+    # Returns a list with all the functions
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
+
 
 
 ## Write a short comment describing this function
